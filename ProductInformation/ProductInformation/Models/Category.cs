8f40c9cb82093bb5c731c377b3a756215ea6d1ae
Krisztina Pap <krisztina.pap@outlook.com>
2020-11-04 09:59:00 -0700

    Adds Product and Category classes; Installs EF packages;

---
+++ ProductInformation/ProductInformation/Models/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductInformation.Models
+{
+    [Table("category")]
+    public class Category
+    {
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID
+        {
+            get; set;
+        }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Name
+        {
+            get; set;
+        }
+    }
+}
---
+++ ProductInformation/ProductInformation/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductInformation.Models
+{
+    [Table("product")]
+    public class Product
+    {
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID
+        {
+            get; set;
+        }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Name
+        {
+            get; set;
+        }
+
+        [Column(TypeName = "int(10")]
+        public int CategoryID
+        {
+            get; set;
+        }
+
+        [ForeignKey(nameof(CategoryID))]
+        [InverseProperty(nameof(Models.Category.ID))]
+
+        public virtual 
+    }
+}
