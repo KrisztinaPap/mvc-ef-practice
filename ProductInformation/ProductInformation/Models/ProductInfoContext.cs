fd9f1120720c57c321531fe454344ffb30f0c7d2
Krisztina Pap <krisztina.pap@outlook.com>
2020-11-04 10:18:58 -0700

    Adds ProductInfoContext;

---
+++ ProductInformation/ProductInformation/Models/ProductInfoContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace ProductInformation.Models
+{
+    public class ProductInfoContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                string connection =
+                        "server=localhost;" +
+                        "port=3306;" +
+                        "user=root;" +
+                        "database=mvc_productinfo;";
+                string version = "10.4.14-MariaDB";
+
+                optionsBuilder.UseMySql(connection, x => x.ServerVersion(version));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Name)
+                        .HasCharSet("utf8mb4")
+                        .HasCollation("utf8mb4_general_ci");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.Name)
+                        .HasCharSet("utf8mb4")
+                        .HasCollation("utf8mb4_general_ci");
+            });
+
+        }
+
+    }
+}
